78403a9a4e70aed4996314fd1ac235c77aa99278
sohaib ahmed <sohaib.ahmed@technologyrivers.com>
2015-03-08 22:07:12 +0500

    update setting

--- globalbridgeproject/settings.py
+++ globalbridgeproject/settings.py
@@ -95,12 +95,12 @@ import dj_database_url
 DATABASES = {
 
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'testheruko',
-    'HOST': 'localhost',
-    'PORT': 5432,
-    'USER': 'postgres',
-    'PASSWORD': 'safimatimohid//',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'NAME': 'testheruko',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': 'safimatimohid//',
   },
 
 }
